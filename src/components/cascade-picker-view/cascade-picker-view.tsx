bc08ebebc581c977701da999c88b3aedf44aa237
Cavano <51314472+p697@users.noreply.github.com>
2021-11-26 23:24:41 +0800

    fix: (Picker & CascadePicker) prevent expectedly calling onChange

--- src/components/cascade-picker-view/cascade-picker-view.tsx
+++ src/components/cascade-picker-view/cascade-picker-view.tsx
@@ -1,5 +1,8 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { useDebounceFn } from 'ahooks'
 import PickerView from '../picker-view'
+import type { PickerValue, PickerValueExtend } from '../picker-view'
+import { mergeProps } from '../../utils/with-default-props'
 import { NativeProps } from '../../utils/native-props'
 import type { PickerViewProps } from '../picker-view'
 import type { CascadePickerOption } from '../cascade-picker'
@@ -10,13 +13,36 @@ export type CascadePickerViewProps = Omit<PickerViewProps, 'columns'> & {
   options: CascadePickerOption[]
 } & NativeProps<'--height'>
 
-export const CascadePickerView: FC<CascadePickerViewProps> = props => {
+const defaultProps = {
+  defaultValue: [],
+}
+
+export const CascadePickerView: FC<CascadePickerViewProps> = p => {
+  const props = mergeProps(defaultProps, p)
   const { options, ...pickerProps } = props
   const { depth, subOptionsRecord } = useCascadePickerOptions(options)
+
+  const [value, setValue] = useState<PickerValue[]>(props.defaultValue)
+
+  const { run: debouncedOnChange } = useDebounceFn(
+    (val: PickerValue[], ext: PickerValueExtend) => {
+      pickerProps.onChange?.(val, ext)
+    },
+    {
+      wait: 0,
+      leading: false,
+      trailing: true,
+    }
+  )
 
   return (
     <PickerView
       {...pickerProps}
+      value={value}
+      onChange={(val, ext) => {
+        setValue(val)
+        debouncedOnChange(val, ext)
+      }}
       columns={selected =>
         generateCascadePickerColumns(
           selected as string[],
--- src/components/cascade-picker/cascade-picker.tsx
+++ src/components/cascade-picker/cascade-picker.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
+import { useDebounceFn } from 'ahooks'
 import Picker from '../picker'
 import type { PickerProps } from '../picker'
+import type { PickerValue, PickerValueExtend } from '../picker-view'
+import { mergeProps } from '../../utils/with-default-props'
 import { useCascadePickerOptions } from './use-cascade-picker-options'
 import { generateCascadePickerColumns } from './cascade-picker-utils'
 
@@ -14,13 +17,36 @@ export type CascadePickerProps = Omit<PickerProps, 'columns'> & {
   options: CascadePickerOption[]
 }
 
-export const CascadePicker: FC<CascadePickerProps> = props => {
+const defaultProps = {
+  defaultValue: [],
+}
+
+export const CascadePicker: FC<CascadePickerProps> = p => {
+  const props = mergeProps(defaultProps, p)
   const { options, ...pickerProps } = props
   const { depth, subOptionsRecord } = useCascadePickerOptions(options)
+
+  const [value, setValue] = useState<PickerValue[]>(props.defaultValue)
+
+  const { run: debouncedOnChange } = useDebounceFn(
+    (val: PickerValue[], ext: PickerValueExtend) => {
+      pickerProps.onSelect?.(val, ext)
+    },
+    {
+      wait: 0,
+      leading: false,
+      trailing: true,
+    }
+  )
 
   return (
     <Picker
       {...pickerProps}
+      value={value}
+      onSelect={(val, ext) => {
+        setValue(val)
+        debouncedOnChange(val, ext)
+      }}
       columns={selected =>
         generateCascadePickerColumns(
           selected as string[],
